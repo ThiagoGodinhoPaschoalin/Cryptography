1de13dfd441ada84bfa4b50250cd1644c01222ed
tgptecnology <thiagogodinhopaschoalin@live.com>
2019-05-19 01:56:32 -0300

    Start Libraries

---
+++ Symmetric/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Symmetric
+{
+    public class Class1
+    {
+        private void Teste()
+        {
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            tdes.GenerateIV();
+            tdes.GenerateKey();
+        }
+    }
+}
